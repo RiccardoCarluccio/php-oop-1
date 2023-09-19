d52dc4235685e501e20fd4fad9bd102f2eeba69c
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-19 17:01:01 +0200

    do php in index

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+  require_once __DIR__ . '/classes/Movie.php';
+
+  $movieList = [
+    [
+      'nome' => 'a1',
+    'genere' => 'a2',
+    'descrizione' => 'a3'
+    ],
+    [
+      'nome' => 'b1',
+    'genere' => 'b2',
+    'descrizione' => 'b3'
+    ],
+    [
+      'nome' => 'c1',
+    'genere' => 'c2',
+    'descrizione' => 'c3'
+    ]
+  ]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-oop-1</title>
+</head>
+<body>
+  
+
+
+</body>
+</html>
